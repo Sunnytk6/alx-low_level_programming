a691e7183a3bebd9e9ff526ab45b986e552565fd
sunnytk6 <sunnytk6@gmial.com>
2023-09-21 06:25:35 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,35 +1,24 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
+#include <stdio.h>
 
 /**
- * print_list - function that prints all the elements of a list_t.
- * @h: pointer to the list.
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to the head of the list
  *
- * if str is NULL print [0] (nil).
- * You are allowed to use printf.
- *
- * Return: the number of nodes.
- * @return size_t
+ * Return: the number of nodes
  */
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	size_t i = 0;
 
-	/* while we have a pointer to list */
-	while (h != NULL)
+	while (h)
 	{
-		/* dereference pointer and check if str is null */
-		if (h->str == NULL)
-		{
-			printf("[%d] %s\n", 0, "(nil)");
-		}
-		else /* if not NULL */
-		{
-			printf("[%d] %s\n", h->len, h->str);
-		}
+		if (h->str)
+			printf("[%u] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
 		h = h->next;
-		count++;
+		i++;
 	}
-	return (count);
+	return (i);
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,7 @@
-#ifndef _LISTS_H_
-#define _LISTS_H_
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
 
 /**
  * struct list_s - singly linked list
@@ -21,6 +23,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-void before_main() __attribute__((constructor));
 
-#endif
+#endif /* LISTS_H */
